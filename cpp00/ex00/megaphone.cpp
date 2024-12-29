9461f426685a4cae3d3584695b8a05b243522787
nferrad <nferrad@student.42.fr>
2024-12-29 04:43:43 +0100

    00/00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	for (int j = 1; j < ac; j++)
+		for (int i = 0; av[j][i]; i++)
+			std::cout << (char)toupper(av[j][i]);
+	std::cout << std::endl;
+	return (0);
+}
