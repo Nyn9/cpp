d16d90228f6b68627e795262643be7a547d49076
nferrad <nferrad@student.42.fr>
2024-12-31 01:05:10 +0100

    01/00

---
+++ cpp01/ex00/Zombie.hpp
@@ -0,0 +1,18 @@
+#ifndef ZOMBIE_H
+#define ZOMBIE_H
+
+#include <iostream>
+
+class Zombie
+{
+    public:
+        Zombie();
+		Zombie(std::string name);
+        ~Zombie();
+		void announce( void );
+
+    private:
+		std::string m_name;
+};
+
+#endif
---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "Zombie.hpp"
+
+Zombie* newZombie( std::string name )
+{
+	Zombie *zombie = new Zombie(name);
+	return (zombie);
+}
+
+void randomChump( std::string name )
+{
+	Zombie zombie = Zombie(name);
+	zombie.announce();
+}
+
+int main()
+{
+	Zombie *tony = newZombie("Tony");
+	tony->announce();
+	randomChump("Many");
+	delete tony;
+}
